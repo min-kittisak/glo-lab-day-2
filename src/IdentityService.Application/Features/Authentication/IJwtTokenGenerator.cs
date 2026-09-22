3d863a5534dd3ebc05849421cfa3d1e0a0968bc0
Nont Sanarmart <nont.san1996@gmail.com>
2026-09-23 03:46:55 +0700

    chore(auth-interface): สร้าง interface สำหรับ implement function generate token

---
+++ src/IdentityService.Application/Features/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityService.Application.Features.Authentication
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(
+            Guid userId,
+            string username,
+            string displayName,
+            string emailAddress,
+            string? departmentCode);
+    }
+}
