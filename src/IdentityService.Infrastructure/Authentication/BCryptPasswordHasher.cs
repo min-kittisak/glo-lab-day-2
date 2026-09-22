b95c66a2b400881fe8c4f134e3a84521c79665db
Nont Sanarmart <nont.san1996@gmail.com>
2026-09-23 03:35:02 +0700

    feat(passwordHash): สร้าง function ในการเข้ารหัสด้วย Bcrypt

---
+++ src/IdentityService.Infrastructure/Authentication/BCryptPasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using IdentityService.Application.Common.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityService.Infrastructure.Authentication
+{
+    public sealed class BCryptPasswordHasher : IPasswordHasher
+    {
+        public bool Verify(
+            string password,
+            string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+    }
+}
