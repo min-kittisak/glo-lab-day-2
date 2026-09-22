0863ca2406cf9b1df2c571218365f9f759f838b5
Nont Sanarmart <nont.san1996@gmail.com>
2026-09-23 03:44:38 +0700

    feat(auth-service): implement function สำหรับ Login

---
+++ src/IdentityService.Application/Features/Authentication/Interfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using IdentityService.Application.Features.Authentication.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityService.Application.Features.Authentication.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<LoginResponse> LoginAsync(
+            LoginRequest request,
+            CancellationToken cancellationToken = default);
+    }
+}
