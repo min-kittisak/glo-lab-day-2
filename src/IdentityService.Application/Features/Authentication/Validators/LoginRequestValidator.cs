14f974a134cddea28dc61e7f65f325f38cee9fbe
Nont Sanarmart <nont.san1996@gmail.com>
2026-09-23 03:45:11 +0700

    chore(login-validator): implemt validator สำหรับการ login

---
+++ src/IdentityService.Application/Features/Authentication/Validators/LoginRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using IdentityService.Application.Features.Authentication.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityService.Application.Features.Authentication.Validators
+{
+    public sealed class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty()
+                .WithMessage("กรุณาระบุชื่อผู้ใช้งาน");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("กรุณาระบุรหัสผ่าน");
+        }
+    }
+}
