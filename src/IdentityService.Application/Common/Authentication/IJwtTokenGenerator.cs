61c4e627c4dada7fba54210f50d44f071159545a
Nont Sanarmart <nont.san1996@gmail.com>
2026-09-23 03:40:06 +0700

    chore(auth-interface);สร้าง interface สำหรับ implement function เข้ารหัสและ generate token

---
+++ src/IdentityService.Application/Common/Authentication/IJwtTokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityService.Application.Common.Authentication
+{
+    internal interface IJwtTokenGenerator
+    {
+    }
+}
---
+++ src/IdentityService.Application/Common/Authentication/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityService.Application.Common.Authentication
+{
+    public interface IPasswordHasher
+    {
+        bool Verify(string password, string passwordHash);
+
+        string Hash(string password);
+    }
+}
